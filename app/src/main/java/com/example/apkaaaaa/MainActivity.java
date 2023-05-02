2fffb358e14997b2fbd0b7aa7fba6606bed5e3da
domipaj <125757111+dxmi13@users.noreply.github.com>
2023-05-02 16:42:21 +0200

    dziala baza

--- app/src/main/java/com/example/apkaaaaa/MainActivity.java
+++ app/src/main/java/com/example/apkaaaaa/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.apkaaaaa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,6 +10,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
         //createDatabase();
         //insertData("K", "Anna", "Nowak", "01-01-1990", "123456789", true, false, true, true);
         getData();
+
+        Button zapiszButton = findViewById(R.id.zapis);
+        zapiszButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wpisane_dane();
+            }
+        });
+
     }
 
     public void createDatabase() {
@@ -57,30 +71,30 @@ public class MainActivity extends AppCompatActivity {
             myDatabase = this.openOrCreateDatabase("myDatabase", MODE_PRIVATE, null);
             cursor = myDatabase.rawQuery("SELECT * FROM myTable", null);
 
-            int idColumn = cursor.getColumnIndex("id");
-            int genderColumn = cursor.getColumnIndex("plec");
-            int firstNameColumn = cursor.getColumnIndex("imie");
-            int lastNameColumn = cursor.getColumnIndex("nazwisko");
-            int birthDateColumn = cursor.getColumnIndex("data_urodzenia");
-            int phoneNumberColumn = cursor.getColumnIndex("telefon");
-            int hasWatchedMovieColumn = cursor.getColumnIndex("filmy");
-            int hasReadLiteratureColumn = cursor.getColumnIndex("literatura");
-            int hasListenedToMusicColumn = cursor.getColumnIndex("muzyka");
-            int isInterestedInSportsColumn = cursor.getColumnIndex("sport");
+            int kolumnaID = cursor.getColumnIndex("id");
+            int kolumnaPlci = cursor.getColumnIndex("plec");
+            int imieKolumna = cursor.getColumnIndex("imie");
+            int nazwiskoKolumna = cursor.getColumnIndex("nazwisko");
+            int dataKolumna = cursor.getColumnIndex("data_urodzenia");
+            int telefonKolumna = cursor.getColumnIndex("telefon");
+            int filmyKolumna = cursor.getColumnIndex("filmy");
+            int literaturaKolumna = cursor.getColumnIndex("literatura");
+            int muzykaKolumna = cursor.getColumnIndex("muzyka");
+            int sportKolumna = cursor.getColumnIndex("sport");
 
             while (cursor.moveToNext()) {
-                int id = cursor.getInt(idColumn);
-                String gender = cursor.getString(genderColumn);
-                String firstName = cursor.getString(firstNameColumn);
-                String lastName = cursor.getString(lastNameColumn);
-                String birthDate = cursor.getString(birthDateColumn);
-                String phoneNumber = cursor.getString(phoneNumberColumn);
-                boolean hasWatchedMovie = cursor.getInt(hasWatchedMovieColumn) == 1;
-                boolean hasReadLiterature = cursor.getInt(hasReadLiteratureColumn) == 1;
-                boolean hasListenedToMusic = cursor.getInt(hasListenedToMusicColumn) == 1;
-                boolean isInterestedInSports = cursor.getInt(isInterestedInSportsColumn) == 1;
+                int id = cursor.getInt(kolumnaID);
+                String plec = cursor.getString(kolumnaPlci);
+                String imie = cursor.getString(imieKolumna);
+                String nazwisko = cursor.getString(nazwiskoKolumna);
+                String data = cursor.getString(dataKolumna);
+                String telefon = cursor.getString(telefonKolumna);
+                boolean filmy = cursor.getInt(filmyKolumna) == 1;
+                boolean literatura = cursor.getInt(literaturaKolumna) == 1;
+                boolean muzyka = cursor.getInt(muzykaKolumna) == 1;
+                boolean sport = cursor.getInt(sportKolumna) == 1;
 
-                Log.d("Database", "ID: " + id + ", Płeć: " + gender + ", Imie: " + firstName + ", Nazwisko: " + lastName + ", Data urodzenia: " + birthDate + ", Telefon: " + phoneNumber + ", Filmy: " + hasWatchedMovie + ", Literatura: " + hasReadLiterature + ", Muzyka: " + hasListenedToMusic + ", Sport: " + isInterestedInSports);
+                Log.d("Database", "ID: " + id + ", Płeć: " + plec + ", Imie: " + imie + ", Nazwisko: " + nazwisko + ", Data urodzenia: " + data + ", Telefon: " + telefon + ", Filmy: " + filmy + ", Literatura: " + literatura + ", Muzyka: " + muzyka + ", Sport: " + sport);
             }
         } finally {
             if (cursor != null) {
@@ -135,7 +149,54 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    private void wpisane_dane(){
+    private void zapisz_do_bazy(String deklaracja_plci, String imie, String nazwisko, String telefon, String dataUrodzenia, boolean lubiFilmy, boolean lubiLiterature, boolean lubiMuzyke, boolean lubiSport){
+        String toastMessage = deklaracja_plci + " " + imie + " " + nazwisko + " " + telefon + " " + dataUrodzenia + " " + lubiFilmy + " " + lubiLiterature + " "+ lubiMuzyke + " " + lubiSport;
+        Log.d("aaaaa", toastMessage);
 
+        try{
+            insertData(deklaracja_plci, imie, nazwisko, dataUrodzenia, telefon, lubiFilmy, lubiLiterature, lubiMuzyke, lubiSport);
+            Toast.makeText(getApplicationContext(), "Dane zostały zapisane", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    private void wpisane_dane(){
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_group);
+
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        String deklaracja_plci = "";
+
+        if (selectedId == R.id.mezczyzna) {
+            deklaracja_plci = "M";
+        } else if (selectedId == R.id.kobieta) {
+            deklaracja_plci = "K";
+        }
+
+        EditText imieET = (EditText) findViewById(R.id.imie);
+        EditText nazwiskoET = (EditText) findViewById(R.id.nazwisko);
+        EditText dataUrodzeniaET = findViewById(R.id.data_urodzenia);
+        EditText telefonET = findViewById(R.id.telefon);
+
+        String imie = imieET.getText().toString();
+        String nazwisko = nazwiskoET.getText().toString();
+        String dataUrodzenia = dataUrodzeniaET.getText().toString();
+        String telefon = telefonET.getText().toString();
+
+        CheckBox filmCheckBox = findViewById(R.id.filmy);
+        CheckBox literaturaCheckBox = findViewById(R.id.literatura);
+        CheckBox muzykaCheckBox = findViewById(R.id.muzyka);
+        CheckBox sportCheckBox = findViewById(R.id.sport);
+
+        boolean lubiFilmy = filmCheckBox.isChecked();
+        boolean lubiLiterature = literaturaCheckBox.isChecked();
+        boolean lubiMuzyke = muzykaCheckBox.isChecked();
+        boolean lubiSport = sportCheckBox.isChecked();
+        zapisz_do_bazy(deklaracja_plci, imie, nazwisko, telefon, dataUrodzenia, lubiFilmy, lubiLiterature, lubiMuzyke, lubiSport);
+    }
+
+
+
+
+
 }
